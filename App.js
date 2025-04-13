8b6595eb5a01c7a968c7f4a0eb39a6eefc030f22
SachinSinghYadav <sachin.singh@gocomet.com>
2025-04-13 13:47:02 +0530

    Nested Structure using React.createElement

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-const Message = ({ message, children }) => {
-  return React.createElement("h1", { id: "heading" }, message, children);
-};
+/*
+    <div id='parent'>
+    
+        <div id='child1'>
+            <h1>
+                H1 Tag
+            </h1>
+            <h2>
+                H1 Tag
+            </h2>
+        </div>
 
-const Heading = React.createElement(
-  Message,
-  { message: "Hello, React" },
-  React.createElement("div", {}, "I am learning you in details")
-);
+        <div id='child2'>
+            <h1>
+                H1 Tag
+            </h1>
+            <h2>
+                H1 Tag
+            </h2>
+        </div>
+
+    </div>
+
+ */
+
+const NestedStructure = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "H1 Tag"),
+    React.createElement("h2", {}, "H2 Tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "H1 Tag"),
+    React.createElement("h2", {}, "H2 Tag"),
+  ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(Heading);
+root.render(NestedStructure);
